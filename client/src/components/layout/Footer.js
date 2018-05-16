767789e365fd280170899837c6451f0b67b9fb5f
masila <abelmasila@gmail.com>
2018-05-16 11:38:15 +0300

    Initial dumb components setup

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <footer className="bg-dark text-white mt-5 p-4 text-center footer">
+        Copyright &copy; {new Date().getFullYear()} Dev-Connect KE
+      </footer>
+    </div>
+  );
+};
