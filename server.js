ba3357efade7b6bf59d4712f8ca5c75dfe976ad0
masila <abelmasila@gmail.com>
2018-06-14 11:17:34 +0300

    Deployment workflow done

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use('/api/posts', posts);
 //serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   //set static folder
-  app.use(express.static('/client/build'));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
