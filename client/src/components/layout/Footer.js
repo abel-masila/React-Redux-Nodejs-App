329e86e2d8582fff84bc7eafdac442182edbd913
masila <abelmasila@gmail.com>
2018-06-14 12:06:58 +0300

    Fixed footer margingBottom

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default () => {
   return (
-    <div>
+    <div style={{ marginBottom: '70px' }}>
       <footer className="bg-dark footer text-white text-center fixed-bottom">
         <p>
           Copyright &copy; {new Date().getFullYear()} {'{Connect - Ke}'}{' '}
